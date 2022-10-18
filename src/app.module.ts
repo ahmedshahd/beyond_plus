a7575a527397dd2ed0688a24516a20fece30d3ea
Ali Gamal <ali.gamal@beyond-solution.com>
2022-10-18 13:29:38 +0200

    NF: install and create graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { GraphQLModule } from '@nestjs/graphql';
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useFactory: () => ({
+        typePaths: ['./**/*.graphql'],
+        definitions: {
+          path: join(process.cwd(), 'src/graphql.ts'),
+          outputAs: 'class',
+        },
+        playground: process.env.NODE_ENV === 'production' ? true : false,
+        introspection: true,
+        csrfPrevention: true,
+        cache: 'bounded',
+        plugins: [
+          process.env.NODE_ENV !== 'production'
+            ? ApolloServerPluginLandingPageLocalDefault()
+            : {},
+        ],
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
