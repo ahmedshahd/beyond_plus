4e5d6d8e29b65ea5103051033fd4ed42bdd2f214
ahmedshahd <ahmedshahd350@gmail.com>
2023-02-18 14:45:40 +0200

    removing api-key

--- src/middlewares/api-key.middleware.ts
+++ src/middlewares/api-key.middleware.ts
@@ -6,6 +6,33 @@ import {
 } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
+// @Injectable()
+// export class ApiKeyMiddleware implements NestMiddleware {
+//   use(req: Request, res: Response, next: NextFunction) {
+//     const apiKey = req.header?.('api-key');
+
+//     if (req?.url?.includes('/graphql')) {
+//       return next();
+//     }
+//     if (req?.url?.includes('/bull')) {
+//       return next();
+//     }
+//     if (req?.url?.includes('/upload')) {
+//       return next();
+//     }
+
+//     if (!apiKey) {
+//       throw new HttpException('api key not found', HttpStatus.UNAUTHORIZED);
+//     }
+
+//     if (apiKey != process.env.API_KEY) {
+//       throw new HttpException('api key is wrong', HttpStatus.UNAUTHORIZED);
+//     }
+
+//     next();
+//   }
+// }
+
 @Injectable()
 export class ApiKeyMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
@@ -21,13 +48,13 @@ export class ApiKeyMiddleware implements NestMiddleware {
       return next();
     }
 
-    if (!apiKey) {
-      throw new HttpException('api key not found', HttpStatus.UNAUTHORIZED);
-    }
+    // if (!apiKey) {
+    //   throw new HttpException('api key not found', HttpStatus.UNAUTHORIZED);
+    // }
 
-    if (apiKey != process.env.API_KEY) {
-      throw new HttpException('api key is wrong', HttpStatus.UNAUTHORIZED);
-    }
+    // if (apiKey != process.env.API_KEY) {
+    //   throw new HttpException('api key is wrong', HttpStatus.UNAUTHORIZED);
+    // }
 
     next();
   }
